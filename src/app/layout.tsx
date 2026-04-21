7f73e3d693eacf7995b9343f47dc413e9b9aee50
Kritika-Panwar-151 <kritikapanwar.cs24@bmsce.ac.in>
2026-04-21 21:06:20 +0530

    ac

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=1200, user-scalable=no" />
-      </head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
       <body>
         <Header />
         {children}
